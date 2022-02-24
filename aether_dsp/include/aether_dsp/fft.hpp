a1617de1cdcafde880851e80d11108b6392d9618
Rui Oliveira <ruimail24@gmail.com>
2022-02-24 20:47:40 +0000

    Tune the fft interface

--- aether_dsp/include/aether_dsp/fft.hpp
+++ aether_dsp/include/aether_dsp/fft.hpp
@@ -1,15 +1,38 @@
 #pragma once
 
 #include "aether_dsp/api.hpp"
+#include "aether_dsp/types.hpp"
 
-#include <complex>
-#include <cstddef>
-#include <span>
+#include <functional>
 
-class AETHER_DSP_API Radix2Fft
+namespace aether_dsp::fft
+{
+
+namespace detail
+{
+
+using fft_impl_t =
+    std::function<void(types::fcomplex_span_t input, types::fcomplex_span_t output,
+                       types::fcomplex_span_t twiddleFactors)>;
+
+} // namespace detail
+
+class AETHER_DSP_API Fft
 {
   public:
-    explicit Radix2Fft(std::size_t size);
-    void operator()(std::span<std::complex<float>> input,
-                    std::span<std::complex<float>> output);
+    enum direction_t
+    {
+        forward,
+        inverse
+    };
+    explicit Fft(std::size_t size, direction_t direction = forward);
+    void operator()(types::fcomplex_span_t input, types::fcomplex_span_t output);
+
+  private:
+    std::size_t size_;
+    direction_t direction_;
+    detail::fft_impl_t fft_impl_;
+    types::fcomplex_buffer_t twiddle_factors_;
 };
+
+} // namespace aether_dsp::fft
--- aether_dsp/src/fft/fft.cpp
+++ aether_dsp/src/fft/fft.cpp
@@ -1,1 +1,36 @@
 #include "aether_dsp/fft.hpp"
+
+namespace aether_dsp::fft
+{
+
+namespace detail
+{
+
+fft_impl_t chooseFftImpl([[maybe_unused]] std::size_t size,
+                         [[maybe_unused]] Fft::direction_t direction)
+{
+    // TODO: Implement this function
+    return {};
+}
+
+types::fcomplex_buffer_t computeTwiddleFactors(
+    [[maybe_unused]] std::size_t size, [[maybe_unused]] Fft::direction_t direction)
+{
+    // TODO: Implement this function
+    return {};
+}
+
+} // namespace detail
+
+Fft::Fft(std::size_t size, direction_t direction)
+    : size_{size},
+      direction_{direction},
+      fft_impl_{detail::chooseFftImpl(size, direction)},
+      twiddle_factors_{detail::computeTwiddleFactors(size, direction)} {};
+
+void Fft::operator()(types::fcomplex_span_t input, types::fcomplex_span_t output)
+{
+    fft_impl_(input, output, twiddle_factors_);
+}
+
+} // namespace aether_dsp::fft
