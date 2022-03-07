35c0f99fca306f209d869fd949db80ea28b3a8aa
Rui Oliveira <ruimail24@gmail.com>
2022-03-07 17:14:48 +0000

    Fix wrong namespace and missing export macro

--- aether_dsp/src/numbers/is_power_of_two.cpp
+++ aether_dsp/src/numbers/is_power_of_two.cpp
@@ -1,11 +1,11 @@
 #include "aether_dsp/numbers.hpp"
 
-namespace aether_sdr::numbers
+namespace aether_dsp::numbers
 {
 
-bool isPowerOfTwo(std::size_t value)
+AETHER_DSP_API bool isPowerOfTwo(std::size_t value)
 {
     return (value > 0 && ((value & (value - 1)) == 0));
 }
 
-} // namespace aether_sdr::numbers
+} // namespace aether_dsp::numbers
