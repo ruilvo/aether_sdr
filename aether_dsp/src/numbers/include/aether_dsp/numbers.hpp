7232e0be0c5fb9ed2a7ce19592af4957e03bdff4
Rui Oliveira <ruimail24@gmail.com>
2022-03-19 15:12:45 +0000

    Document the functions in ::numbers

--- aether_dsp/src/numbers/include/aether_dsp/numbers.hpp
+++ aether_dsp/src/numbers/include/aether_dsp/numbers.hpp
@@ -6,8 +6,22 @@
 namespace aether_dsp::numbers
 {
 
+/**
+ * @brief Check whether a number is a power of 2.
+ *
+ * @param value the number to check the value of.
+ * @return bool true if the number is a power of 2, false otherwise. 0 returns false.
+ */
 AETHER_DSP_API bool isPowerOfTwo(std::size_t value);
 
+/**
+ * @brief Reverse the bit representation of a number, considering the
+ * bit_width(maximum_value) as the number of bits.
+ *
+ * @param value value to reverse the bit representation of.
+ * @param maximum_value value used to computed the bit-width to use.
+ * @return std::size_t the reversed bit representation of the value.
+ */
 AETHER_DSP_API std::size_t reverseBits(std::size_t value, std::size_t maximum_value);
 
 } // namespace aether_dsp::numbers
