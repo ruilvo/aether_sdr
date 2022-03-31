0fc0791e50da2ff0eb3e1e6ba8e45d780c4201e1
Rui Oliveira <ruimail24@gmail.com>
2022-03-31 20:22:56 +0100

    Make MSVC be more C++ compliant

--- cmake/compiler_flags.cmake
+++ cmake/compiler_flags.cmake
@@ -1,5 +1,10 @@
 include_guard(GLOBAL)
 
+if(NOT AETHER_USE_ISO_FLAGS)
+  return()
+endif()
+
 if(MSVC)
-  add_compile_options("/EHsc")
+  add_compile_options("/EHsc" "/volatile:iso" "/Zc:preprocessor" "/Zc:throwingNew"
+                      "/Zc:externConstexpr")
 endif()
