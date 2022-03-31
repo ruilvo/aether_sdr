c34ac0936d83ac31420f647481242378005676ac
Rui Oliveira <ruimail24@gmail.com>
2022-03-31 20:22:09 +0100

    Use `PROJECT_IS_TOP_LEVEL` to decide whether to set install options.

--- cmake/install_structure.cmake
+++ cmake/install_structure.cmake
@@ -3,7 +3,7 @@ include_guard(GLOBAL)
 # Canonical install dirs
 include(GNUInstallDirs)
 
-if(NOT WIN32 AND CMAKE_SOURCE_DIR STREQUAL CMAKE_CURRENT_SOURCE_DIR)
+if(NOT WIN32 AND PROJECT_IS_TOP_LEVEL)
   set(CMAKE_INSTALL_PREFIX "/opt/aether_sdr")
   set(CPACK_PACKAGING_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 endif()
