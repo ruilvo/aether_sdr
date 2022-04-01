5212a89defba2074631c24b9619b963cae139682
Rui Oliveira <ruimail24@gmail.com>
2022-04-01 15:41:45 +0100

    Back to a clean sheet design

--- aether_dsp/include/aether_dsp/fft.hpp
+++ aether_dsp/include/aether_dsp/fft.hpp
@@ -1,45 +1,24 @@
 #pragma once
 
 #include "aether_dsp/api.hpp"
-#include "aether_dsp/macros.hpp"
 #include "aether_dsp/types.hpp"
 
-#include <memory>
+#include <cstddef>
 
 namespace aether_dsp::fft
 {
 
-namespace impl
+enum class direction_t
 {
-
-class IFftImpl
-{
-  public:
-    IFftImpl() = default;
-    AETHER_DSP_NO_COPY_MOVE(IFftImpl)
-    virtual ~IFftImpl() = default;
-
-    virtual void operator()(std::span<const std::complex<float>> input,
-                            std::span<std::complex<float>> output) = 0;
+    forward,
+    backward
 };
 
-} // namespace impl
-
-class AETHER_DSP_API Fft
+class Fft
 {
   public:
-    enum direction_t
-    {
-        forward,
-        inverse
-    };
-    explicit Fft(std::size_t size, direction_t direction = forward);
-    void operator()(std::span<const std::complex<float>> input,
-                    std::span<std::complex<float>> output);
-
-  private:
-    AETHER_DSP_SUPPRESS_C4251
-    std::unique_ptr<impl::IFftImpl> fft_impl_;
+    Fft() = delete;
+    explicit Fft(std::size_t size, direction_t direction);
 };
 
 } // namespace aether_dsp::fft
