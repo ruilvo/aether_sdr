e19b04c9852cf3cb845d556252d179eb095b876f
Rui Oliveira <ruimail24@gmail.com>
2022-04-02 13:38:16 +0100

    Add `AETHER_DSP_DEFAULT_COPY_MOVE`

--- aether_dsp/include/aether_dsp/macros.hpp
+++ aether_dsp/include/aether_dsp/macros.hpp
@@ -6,6 +6,12 @@
     classname &operator=(const classname &) = delete; \
     classname &operator=(classname &&) = delete;
 
+#define AETHER_DSP_DEFAULT_COPY_MOVE(classname)        \
+    classname(const classname &) = default;            \
+    classname(classname &&) = default;                 \
+    classname &operator=(const classname &) = default; \
+    classname &operator=(classname &&) = default;
+
 #if defined(_MSC_VER) && !defined(__ICL)
     #define AETHER_DSP_SUPPRESS_C4251 _Pragma("warning(suppress:4251)")
 #else
