c3ae76f6ec60ffdf6d56637c9074fdfc98e94ae5
Rui Oliveira <ruimail24@gmail.com>
2022-04-02 13:38:07 +0100

    Remove disabling of check that's not longer enforced

--- aether_dsp/include/aether_dsp/literals.hpp
+++ aether_dsp/include/aether_dsp/literals.hpp
@@ -6,7 +6,6 @@ namespace aether_dsp::literals
 {
 
 // https://stackoverflow.com/a/22346651
-// NOLINTNEXTLINE(google-runtime-int)
 constexpr std::size_t operator"" _sz(unsigned long long value) noexcept
 {
     return static_cast<std::size_t>(value);
