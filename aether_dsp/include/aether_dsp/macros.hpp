23ab1bd597ef8f20505d7e87ec2466b02a10e32a
Rui Oliveira <ruimail24@gmail.com>
2022-04-15 18:30:51 +0100

    Touch up available macros

--- aether_dsp/include/aether_dsp/macros.hpp
+++ aether_dsp/include/aether_dsp/macros.hpp
@@ -1,19 +1,25 @@
 #pragma once
 
-#define AETHER_DSP_NO_COPY_MOVE(classname)            \
-    classname(const classname &) = delete;            \
-    classname(classname &&) = delete;                 \
-    classname &operator=(const classname &) = delete; \
+#define AETHER_DSP_NO_MOVE(classname) \
+    classname(classname &&) = delete; \
     classname &operator=(classname &&) = delete;
 
-#define AETHER_DSP_DEFAULT_COPY_MOVE(classname)        \
-    classname(const classname &) = default;            \
-    classname(classname &&) = default;                 \
-    classname &operator=(const classname &) = default; \
+#define AETHER_DSP_NO_COPY(classname)      \
+    classname(const classname &) = delete; \
+    classname &operator=(const classname &) = delete;
+
+#define AETHER_DSP_NO_COPY_MOVE(classname) \
+    AETHER_DSP_NO_MOVE(classname)          \
+    AETHER_DSP_NO_COPY(classname)
+
+#define AETHER_DSP_DEFAULT_MOVE(classname) \
+    classname(classname &&) = default;     \
     classname &operator=(classname &&) = default;
 
-#if defined(_MSC_VER) && !defined(__ICL)
-    #define AETHER_DSP_SUPPRESS_C4251 _Pragma("warning(suppress:4251)")
-#else
-    #define AETHER_DSP_SUPPRESS_C4251
-#endif
+#define AETHER_DSP_DEFAULT_COPY(classname)  \
+    classname(const classname &) = default; \
+    classname &operator=(const classname &) = default;
+
+#define AETHER_DSP_DEFAULT_COPY_MOVE(classname) \
+    AETHER_DSP_DEFAULT_MOVE(classname)          \
+    AETHER_DSP_DEFAULT_COPY(classname)
